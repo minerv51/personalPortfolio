8292131aa0dfc2447d63b7b95e709a7eda2ebedb
minerv51 <bam0119@hotmail.com>
2024-11-29 23:37:21 -0500

    Accessibility+

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ function Footer () {
         <div className='footer'>
             <p>&copy; 2024 Ben Anthony Minervino. All rights reserved.</p>
             <div className='footerLinks'>
-                <a href='https://www.linkedin.com/in/ba-minervino' target='_blank'><FiLinkedin /></a>
-                <a href='https://www.instagram.com/minerv.51/' target='_blank'><FiInstagram /></a>
-                <a href='https://github.com/minerv51' target='_blank'><FiGithub /></a>
+                <a href='https://www.linkedin.com/in/ba-minervino' target='_blank' aria-label='Opens in a new tab'><FiLinkedin /></a>
+                <a href='https://www.instagram.com/minerv.51/' target='_blank' aria-label='Opens in a new tab'><FiInstagram /></a>
+                <a href='https://github.com/minerv51' target='_blank' aria-label='Opens in a new tab'><FiGithub /></a>
             </div>
         </div>
     )

c008423b6b8e8632f0702a5b8a5dc4993ab77595
minerv51 <bam0119@hotmail.com>
2025-01-27 15:04:02 -0500

    EmailJS Fix Attempt 1

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -29,9 +29,9 @@ const ContactForm = () => {
             const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
             const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
             const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-            //console.log(import.meta.env.VITE_EMAILJS_SERVICE_ID);
-            //console.log(import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
-            //console.log(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+            console.log(import.meta.env.VITE_EMAILJS_SERVICE_ID);
+            console.log(import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
+            console.log(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 
             emailjs

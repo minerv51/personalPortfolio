a6885a9dc272592d713410a4f48de734cefa9d6d
minerv51 <bam0119@hotmail.com>
2025-01-27 15:18:00 -0500

    EmailJS Fix Attempt 2

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/personalPortfolio/',
+
+  build: {
+    minify: false, //Prevent minification for debugging
+  },
 })

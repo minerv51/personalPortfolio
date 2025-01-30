51155ed4ccff5b6d41741a77b62d95c7cfa3bfa4
minerv51 <bam0119@hotmail.com>
2025-01-29 21:29:09 -0500

    Added cool button

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -131,7 +131,15 @@ const ContactForm = () => {
 
                     <div className="consent-button">
                         <label htmlFor="consent">I agree to be contacted for a follow-up</label>
-                        <input required id="consent" name="consent" type="checkbox" checked={formData.consent} onChange={handleChange} />
+                        <div className="container">
+                            <input required name="consent" checked={formData.consent} onChange={handleChange} type="checkbox" id="cbx2" style={{ display: 'none' }} />
+                            <label htmlFor="cbx2" className="check">
+                                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                                    <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+                                    <polyline points="1 9 7 14 15 4"></polyline>
+                                </svg>
+                            </label>
+                        </div>
                     </div>
 
                     <div className="submit-reset">

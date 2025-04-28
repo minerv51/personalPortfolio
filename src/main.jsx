140d2a9c3e9814454d7e25f5da820b248e0d80b2
minerv51 <bam0119@hotmail.com>
2025-04-27 22:19:08 -0400

    Added Vercel Speed Insights & Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Router'
 import { HashRouter as Router } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.render(
   <Router>
+    <>
       <App />
+      <Analytics /> {/* Vercel Analytics */}
+      <SpeedInsights /> {/* Vercel Speed Insights */}
+    </>
   </Router>,
   document.getElementById('root')
 );

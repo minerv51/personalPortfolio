4f2989ee3f840172bdf68de3f024583f1af6d57f
minerv51 <bam0119@hotmail.com>
2025-06-01 23:18:33 -0400

    Resume page revamp

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,4 +1,10 @@
 import '../styles/Resume.css'
+import { AiOutlineClockCircle } from 'react-icons/ai'
+import { FaUserTie, FaLaptopCode, FaRegLightbulb, FaBolt, FaChartPie } from 'react-icons/fa'
+import { HiOutlineChartBar} from 'react-icons/hi'
+import { GiMuscleUp } from 'react-icons/gi'
+import { MdComputer } from 'react-icons/md'
+import { SiPython } from 'react-icons/si'
 
 function ResumeSection () {
 
@@ -6,13 +12,7 @@ function ResumeSection () {
         <div className='resumeSection'>
             <div className='resumeTitle'>
                 <h1>My Resume</h1>
-                <p>Below, you can view an online copy of my resume and download a PDF version using the button at the bottom of the page.</p>
-            </div>
-            <div className='resumeFile'>
-                <iframe className='iframe' 
-                    src='https://drive.google.com/file/d/1NSTrvCQE1tB7tW77dBftIZVSHUa0J2nR/preview'
-                    title='Resume Preview'
-                />
+                <p>Here you’ll find a summary of my experience, education, and skills. You can view my resume below or download the PDF version.</p>
             </div>
             <div className='resumeDownload'>
                 <a href='/resume.pdf' download='baminervino_resume.pdf'>
@@ -20,6 +20,51 @@ function ResumeSection () {
                         Download Resume
                     </button>
                 </a>
+            </div>
+            <div className='skillsContent'>
+                <h1>My Key Skills</h1>
+                <div className='skillsList'>
+                    <div className='skillItem'>
+                        <AiOutlineClockCircle className='skillIcon' />
+                        <h3 className='skillText'>Time Management & Organization</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <FaUserTie className='skillIcon' />
+                        <h3 className='skillText'>Leadership & Team Collaboration</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <HiOutlineChartBar className='skillIcon' />
+                        <h3 className='skillText'>Project Management</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <GiMuscleUp className='skillIcon' />
+                        <h3 className='skillText'>Hard-Working, Determined, & Ambitious</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <FaLaptopCode className='skillIcon' />
+                        <h3 className='skillText'>Frontend Web Design & Development</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <FaRegLightbulb className='skillIcon' />
+                        <h3 className='skillText'>Creative Problem Solving</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <FaBolt className='skillIcon' />
+                        <h3 className='skillText'>Adaptability & Quick Learning</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <MdComputer className='skillIcon' />
+                        <h3 className='skillText'>Microsoft Office Proficiency</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <SiPython className='skillIcon' />
+                        <h3 className='skillText'>Python Programming</h3>
+                    </div>
+                    <div className='skillItem'>
+                        <FaChartPie className='skillIcon' />
+                        <h3 className='skillText'>Data Analysis & Visualization Through Tableau</h3>
+                    </div>
+                </div>
             </div>
         </div>
     )

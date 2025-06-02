08fe6f1451e08c30497f890090874ad161ba83c8
minerv51 <bam0119@hotmail.com>
2025-06-02 14:54:01 -0400

    Changed resume and about styling

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import ResumeSection from '../components/Resume.jsx'
+import Experience from '../components/Experience.jsx'
+import Education from '../components/Education.jsx'
 
 function Resume() {
   return (
     <StrictMode>
       <ResumeSection />
+      <Experience />
+      <Education />
     </StrictMode>
   );
 }
